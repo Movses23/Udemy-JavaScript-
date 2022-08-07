f39a392dde1337980902b8b20cd71dffbfef5ee0
Movses <m.sargatyan@yandex.ru>
2022-08-07 15:59:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,107 @@
+"use strict";
+//let number = 5;
+//const LeftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+
+
+//{
+   //  let result = 50;
+//}
+
+
+
+//let nUmber = 4.6;
+ //const persone = 'Alex';
+
+ //const bool = false;
+
+ //let people;
+ //console.log(people);
+
+
+
+
+ //let arrg = ['plum.png', 'orange.jpg', 'car.jpg', {}];
+ //console.log(arrg[1]);
+
+ //const arrObj = {
+    //a: 'a',
+  //'1': 'b',
+    //2: 'c',
+    //abc: {
+        //def:{
+
+        //}
+    //}
+     //};
+ 
+ //const obj = {
+     //'Anna': 500,
+     //'Alice': 800,
+ //};
+ //const arr = ['a', 'b', 'c'];
+
+//arr[10] = '3456';
+
+//console.log(arr);
+
+ //arrObj.b = '1234';
+
+ //console.log(arrObj.b);
+
+//alert('hello!');
+ 
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer+ 5);
+//console.log(typeof (answer));
+
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваше фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+
+//const category = 'toys';
+//console.log (`https://someurl.com/${category}/6`);
+
+//const user = "Ivan";
+//alert(`Привет,${user}`);
+
+//console.log('arr' + "-object");
+
+//console.log (4 + "-object");
+
+//console.log (4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+    //incr ++;
+    //decr --;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log (2+2*4 === 8);
+
+//const isCheked = true;
+       //isClose = false;
+
+//console.log (isCheked && isClose);
+
+//const isCheked = true;
+       //isClose = false;
+
+//console.log (isCheked || !isClose);
+
