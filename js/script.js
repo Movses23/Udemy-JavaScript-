97d8322a2b6b90b4e0efda797a1e75e872497e1c
Movses <m.sargatyan@yandex.ru>
2022-08-15 14:17:46 +0300

    Third work on app in Javascript

--- js/script.js
+++ js/script.js
@@ -116,12 +116,245 @@ const personalMovieDB = {
  private: false,
 };
 
-const  a = prompt('Один из последних просмотренных фильмов?', ''),
-       b = prompt('На сколько оцените его?', ''),
-       c = prompt('Один из последних просмотренных фильмов?', ''),
-       d = prompt('На сколько оцените его?', '');
+//const  a = prompt('Один из последних просмотренных фильмов?', ''),
+       //b = prompt('На сколько оцените его?', ''),
+       //c = prompt('Один из последних просмотренных фильмов?', ''),
+       //d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+//personalMovieDB.movies [a] = b;
+//personalMovieDB.movies [c] = d;
 
+//console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++ ){
+    const  a = prompt('Один из последних просмотренных фильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
+           personalMovieDB.movies [a] = b;
+           
+if(a != null && b != null && a != '' && b !='' && a.length <= 50){
+    personalMovieDB.movies [a] = b;
+} else{
+console.log('Ответьте заново');
+i--;
+} if (personalMovieDB.count < 10){
+ console.log ('Просмотрено довольно мало фильмов');
+} else if(10 <= personalMovieDB.count >= 30){
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30){
+console.log('Вы киноман');
+}
+
+
+
+
+console.log(personalMovieDB.movies);
+}
+
+
+
+
+
+
+ 
 console.log(personalMovieDB);
+
+//if ( 4 == 9) {
+    //console.log ('ok!');
+//} else {
+    //console.log ('error');
+//}
+
+
+
+//if (num < 49){
+    //console.log('error');
+//} else if(num> 100){
+    //console.log('Много');
+  
+//} else {
+    //console.log('ok!');
+//}
+
+//(num === 50) ? console.log('ok!') : console.log('error');
+
+//const num = 50;
+
+//switch(num) {
+//case 49:
+     //console.log('Неверно');
+     //break;
+     //case 100:
+        //console.log('Неверно');
+        //break;
+//case 50:
+    //console.log('ok');
+//break;
+//default:
+    //console.log('Не в этот раз');
+    //break;
+//}
+
+//const hamBurger = 5;
+//const fries = 0;
+
+//if (hamBurger && fries) {
+    //console.log('Я сыт!');
+//}
+
+//const hamBurger = 3;
+//const fries = 3;
+//const cola = 0;
+//const nuggets = 2;
+
+
+//if (hamBurger === 3 && cola === 2 || fries === 3 && nuggets) {
+    //console.log('Все довольны!');
+//} else {
+    //console.log('Уходим!');
+//}
+
+//console.log(hamBurger === 3 && cola === 2 || fries === 3 && nuggets);
+
+
+//console.log(!0);
+//let JohnReport, alexReport, samReport, mariaReport = 'done';
+//console.log(hamBurger === 3 && cola === 2 || fries === 3 && nuggets);
+//console.log(1 && 0);
+//console.log(1 && 5);
+//console.log(null && 5);
+//console.log(0 && 'gjkfklgmlrg');
+
+//console.log((hamBurger === 3 && cola && fries ));
+
+//const fri = 1;
+//const iqos = 1;
+//const water = 4;
+
+
+//console.log( NaN && 2 && undefined );
+
+
+ 
+//console.log( !1 && 2 || !3 );
+
+//console.log( 25 || null && !3 );
+
+//console.log( NaN || null || !3 || undefined || 5);
+
+ 
+//console.log( NaN || null && !3 && undefined || 5);
+
+//console.log( 5 === 5 && 3 > 1 || 5);
+
+
+//const hamburger = 3;
+//const fries = 1;
+//const cola = 0;
+//const nuggets = 2;
+ 
+//if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   //console.log('Done!');
+//}
+
+
+//let hamburger;
+//const fries = NaN;
+//const cola = 0;
+//const nuggets = 2;
+ 
+//if (hamburger || cola || fries === 3 || nuggets) {
+   //console.log((hamburger || cola || fries === 3 || nuggets));
+//}
+
+
+//let hamburger;
+//const fries = NaN;
+//const cola = 0;
+//const nuggets = 2;
+ 
+//if (hamburger && cola || fries === 3 && nuggets) {
+   //console.log('Done!');
+//}
+
+//let num = 50;
+ 
+//while (num <= 55) {
+    //console.log(num);
+    //num++;
+//}
+
+//do{
+    //console.log(num);
+    //num++; 
+//}
+//while (num < 55);
+
+//for (let i = 1; i < 10; i++){
+    //if ( i === 6) {
+        //break;
+        //continue;
+    //}
+    
+    //console.log(i);
+//}
+
+ //first: for ( let i = 0; i < 3; i++){
+    //console.log(`first level: ${i}`);
+    //for ( let j = 0; j < 3; j++){
+        //console.log(`Second level: ${j}`);
+    //}
+    //for ( let k = 0; k < 3; k++){
+       // if (k === 2) continue first;
+        //console.log(`Third level: ${k}`);
+    //}
+//}
+
+//let result = '';
+//const lenght = 7;
+//for (let i = 1; i < lenght; i++){
+//for (let j = 0; j < i; j++){
+//result += "*";
+//}
+
+    //result += '\n';
+//}
+
+
+
+
+
+//function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    //const data = [5, 10, 'Shopping', 20, 'Homework'];
+//for(let i = 5; i < data.length;  i++){
+    //if(typeof(data[i])`number:${i}`){
+       //data[i] = data[i] * 2;
+    //}
+       //for(let j = 5; j < data.length;  j++){
+     
+     //if(typeof(data[j])`string:${j}`){
+    //data[j] = ('- done');
+    //console.log(data[j]);
+     //} 
+//}
+    //}
+    //return data;
+    
+//}  
+
+//secondTask();
+
+
+
+
+//const lines = 5;
+//let result = '';
+//for(let i = 1; i < lines; i++){
+    //for (let j = 2; j < lines; j++ ){
+        //result += "*";
+    //}
+//} console.log(result);
+
+
+
+//
